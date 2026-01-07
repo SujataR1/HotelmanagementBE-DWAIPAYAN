baadbe401acfd0fb1090d3a8b1776349acfb8e7b
dwaipayantgpl <tramsogrify42@outlook.com>
2026-01-07 16:04:21 +0530

    create the local host and update the whole project

--- server/swagger.js
+++ server/swagger.js
@@ -1,26 +1,50 @@
-const swaggerJSDoc = require("swagger-jsdoc");
+// const swaggerJSDoc = require("swagger-jsdoc");
 
-const options = {
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Hotel Management API",
+//       version: "1.0.0",
+//       description: "Swagger documentation"
+//     },
+//     servers: [
+//       {
+//         url: "http://192.168.0.187:5000"
+//       }
+//     ]
+//   },
+//   apis: ["./auth/*.js"]
+// };
+
+// const swaggerSpace = swaggerJSDoc(options);
+
+// module.exports = swaggerSpace;
+
+
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const swaggerSpec = swaggerJsdoc({
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Hotel Management API",
+      title: "Auth API",
       version: "1.0.0",
-      description: "Swagger documentation"
     },
     servers: [
       {
-        url: "http://localhost:5000"
+        url: "http://localhost:5000",
+        description: "Localhost"
+      },
+      {
+        url: "http://192.168.0.187:5000",
+        description: "LAN / Network"
       }
-    ]
+    ],
   },
-  apis: ["./auth/*.js"]
-};
+  apis: ["./routes/*.js", "./auth/*.js"],
+});
 
-const swaggerSpace = swaggerJSDoc(options);
-
-module.exports = swaggerSpace;
-
-
+module.exports = swaggerSpec;
 
 
